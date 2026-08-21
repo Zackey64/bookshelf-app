2c8f1c67ff32679fc02f193915d409d4fa782b8d
Zackey64 <ishizaki0826@gmail.com>
2026-08-21 14:08:53 +0900

    feat マイグレーションファイル作成 #1

---
+++ database/migrations/2026_08_21_044712_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn', 13);
+            $table->date('published_date');
+            $table->string('image_url')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};

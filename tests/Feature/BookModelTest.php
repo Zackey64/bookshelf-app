b10c5f820a53e8b220a013a7b0a1be53eb778fea
Zackey64 <ishizaki0826@gmail.com>
2026-09-07 22:05:49 +0900

    feat 書籍モデルテスト追加 #24

---
+++ tests/Feature/BookModelTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use App\Models\Genre;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_この書籍を登録したユーザーと１対多の関係を持つ(): void
+    {
+        // Arrange
+        $user = User::factory()->create();
+        $book = Book::factory()->create([
+            'user_id' => $user->id,
+        ]);
+        // Assert
+        $this->assertTrue($book->user->is($user));
+    }
+
+    /** @test */
+    public function genres_ジャンルと多対多の関係を持つ(): void
+    {
+        // Arrange
+        $book = Book::factory()->create();
+        $genre = Genre::factory()->create();
+        $book->genres()->attach($genre);
+        // Assert
+        $this->assertTrue($book->genres->contains($genre));
+    }
+
+    /** @test */
+    public function reviews_レビューと１対多の関係を持つ(): void
+    {
+        // Arrange
+        $book = Book::factory()->create();
+        $review = Review::factory()->create([
+            'book_id' => $book->id,
+        ]);
+        // Assert
+        $this->assertTrue($book->reviews->contains($review));
+    }
+
+    /** @test */
+    public function favorited_by_users_書籍をお気に入り登録しているユーザーと多対多の関係を持つ(): void
+    {
+        // Arrange
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+        $book->favoritedByUsers()->attach($user);
+        // Assert
+        $this->assertTrue($book->favoritedByUsers->contains($user));
+    }
+}

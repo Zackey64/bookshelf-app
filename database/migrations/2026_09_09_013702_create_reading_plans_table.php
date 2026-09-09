0e6e36de024a3e91d70bddbeaf7e612561415ab7
Zackey64 <ishizaki0826@gmail.com>
2026-09-09 10:45:15 +0900

    feat マイグレーション追加 #36

--- database/migrations/2026_09_09_013702_create_reading_plans_table.php
+++ database/migrations/2026_09_09_013702_create_reading_plans_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('reading_plans', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->string('author');
-            $table->string('isbn', 13);
-            $table->date('published_date')->nullable();
-            $table->string('image_url')->nullable();
-            $table->text('description')->nullable();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+            $table->date('target_date');
+            $table->timestamp('completed_at')->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('reading_plans');
     }
 };
